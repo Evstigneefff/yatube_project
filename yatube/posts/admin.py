0384458589905a1a36936011df39ca7a49fa501d
Алексей <evstigneefff@gmail.com>
2022-05-21 20:44:14 +0300

    сохранение

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from .models import Post, Group
 
-from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('text', 'pub_date', 'author') 
-    search_fields = ('text',) 
-    list_filter = ('pub_date',) 
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
-admin.site.register(Post, PostAdmin)  
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    list_fields = ('text',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
